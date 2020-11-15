272d5f253545e378f6e9a393cfc9abcd0b21dfb3
Manuel Stefan Christopher <manuelstefan150@gmail.com>
2020-11-15 17:34:09 +0700

    next

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './Header'
 import Classes from './Classes';
 
 function App() {
+
   return (
     <Router>
       <div className="App">
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
+
     return (
-        <div>
-            
-        </div>
-    )
+        <nav className="header">
+            <Link to="/" className="header__link">
+                <p className="header__logo">RemCol</p>
+            </Link>
+        
+            <div className="header__nav">
+                <Link to="/login" className="header__link"> {/* Redirects if logged in */}
+                    <div className="header__option">
+                        <span className="header__optionLine">{'Sign In'}</span>
+                    </div>
+                </Link>
+            </div>
+        </nav>
+    );
 }
 
 export default Header
