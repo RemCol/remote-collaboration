eca768e2abba9fa2931f507f34f1a856b1f593e1
JeknJok <74481574+JeknJok@users.noreply.github.com>
2020-11-16 03:26:14 +0700

    ignore last push. This is the fixed one. Class stream viewable.

--- src/Class.js
+++ src/Class.js
@@ -8,6 +8,8 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { BrowserRouter as Router, Switch, Route, NavLink} from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import CardActions from '@material-ui/core/CardActions';
 
 const useStyles = makeStyles({
   root: {
