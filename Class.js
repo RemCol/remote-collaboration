652ad611e2bb38ff1958676eaf4cc3d3962acef1
JeknJok <74481574+JeknJok@users.noreply.github.com>
2020-11-15 17:59:07 +0700

    HTML AND CSS SOURCE CODE

---
+++ Class.js
@@ -0,0 +1,84 @@
+const { default: Classes } = require("./src/Classes")
+
+<Classes/>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width">
+    <title>Catalogue class</title>
+    <link href="style.css" rel="stylesheet">
+    <link  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <script src="script.js"></script>
+    <div class="container">
+      <h2>Class List</h2>
+      <div class="row">
+
+
+        <div class="col-md-3">
+          <div class="classroom-top">
+            <img src="images/class2.jpg">
+            <div class ="overlay">
+              <button type="button" class="btn btn-secondary" title="Enter Class Stream"><i class="fa fa-book" ></i></button>
+              <button type="button" class="btn btn-secondary" title="Button"><i class="fa fa-book" ></i></button>
+              <button type="button" class="btn btn-secondary" title="Button"><i class="fa fa-book" ></i></button>              
+            </div>
+            <h3>Math Class</h3>
+            <h5>Mr.Greg</h5>
+          </div>
+        </div>
+
+
+        <div class="col-md-3">
+          <div class="classroom-top">
+            <img src="images/class2.jpg">
+            <div class="overlay">
+              <button type="button" class="btn btn-secondary" title="Enter Class Stream"><i class="fa fa-book" ></i></button>
+              <button type="button" class="btn btn-secondary" title="Button"><i class="fa fa-book" ></i></button>
+              <button type="button" class="btn btn-secondary" title="Button"><i class="fa fa-book" ></i></button>              
+            </div>
+            <h3>English Class</h3>
+            <h5>Ms.Greg</h5>
+          </div>
+
+        </div>
+
+        <div class="col-md-3">
+          <div class="classroom-top">
+            <img src="images/class2.jpg">
+            <div class="overlay">
+              <button type="button" class="btn btn-secondary" title="Enter Class Stream"><i class="fa fa-book" ></i></button>
+              <button type="button" class="btn btn-secondary" title="Button"><i class="fa fa-book" ></i></button>
+              <button type="button" class="btn btn-secondary" title="Button"><i class="fa fa-book" ></i></button>              
+            </div>
+            <h3>Science Class</h3>
+            <h5>Mr.Victor</h5>
+          </div>
+
+        </div>
+
+        <div class="col-md-3">
+          <div class="classroom-top">
+            <img src="images/class2.jpg">
+            <div class="overlay">
+              <button type="button" class="btn btn-secondary" title="Enter Class Stream"><i class="fa fa-book" ></i></button>
+              <button type="button" class="btn btn-secondary" title="Button"><i class="fa fa-book" ></i></button>
+              <button type="button" class="btn btn-secondary" title="Button"><i class="fa fa-book" ></i></button>              
+            </div>
+            <h3>Geography Class</h3>
+            <h5>Mr.Chris</h5>
+          </div>
+
+        </div>
+
+
+
+
+      </div>
+    </div>
+  </body>
+</html>
+
